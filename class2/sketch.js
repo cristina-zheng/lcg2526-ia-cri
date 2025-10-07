cc329b939842ee4e434a9c33e41ab05abe48560f
cristina-zheng <cristina.zheng@mail.polimi.it>
2025-10-07 15:39:53 +0200

    carica

--- class2/sketch.js
+++ class2/sketch.js
@@ -4,79 +4,56 @@ let yMax = 600;
 let xRocket = xMax/2;
 let yRocket = yMax*0.6;
 
-function setup() {
-  createCanvas(xMax, yMax);
-  //rallentare
-  frameRate(5);
+let table;
+let star_img;
+let rocket_img;
+
+function preload() { //funzione eseguita all'inizio che carica tutti i dati
+  table = loadTable("star.csv", "csv", "header");
+  star_img = loadImage("star.png");
+  rocket_img = loadImage("shuttle.png");
 }
 
-function draw() {
-  background("#061b2dff");
-  // mostrare un testo bianco che
-  // dice le coordinate del mouse
-  // sul foglio da disegno
-  fill(0); //bianco
-  textSize(20);
-  //stringa, x, y
-  text("mouseX:"+ mouseX + ", \
-    mouseY: " + mouseY,20,20); // mouseX dice la posizione X del mouse all'interno del foglio
+function setup() { //invocata ogni volta che refresh la pagina
+  createCanvas(xMax, yMax);
+  //rallentare
+  frameRate(10);
+}
 
-  //disegnare le stelle
-  //120
-  //tre tipi di stelle a, b, c
-  //fino a che abbiamo 120 stelle
-  //stelle ellipse
-  push();
-    //3 cicli
+function drawSingleStarFromFile(index, posX, posY) {
+  let starSize = table.getNum(index, "starSize")
+  image(star_img, posX, posY, starSize, starSize);
+}
 
-    //ciclo 1 specifica stella a, 40
-    //noStroke();
-    strokeWeight(random(0, 2))
+function drawStarsFromFile() {
+  for(let k = 0; k < table.getRowCount(); k++) { //k parte da 0 e arriverà fino al numero totale delle righe
+    let starX = (k*37) % width + (k%3) * 5;
+    let starY = (k*73) % height + (k%7);
 
-    //un unico ciclo
-    //creama una sequenza per fare a, b, c
-    for(let i = 0; i < 120; i++){
-      //let starX = (i*37) % width + (i%3) * 5; //posizione stella collegata
-      // al valore dell'indice
-      // width è la larghezza dello schermo
-      //let starY = ((i*73) % height) + (i%7);
+    drawSingleStarFromFile(k, starX, starY);
+  }
+}
 
-      //la posizione delle stelle random
-      let starX = random(0, 500);
-      let starY = random(0, 800);
-      //operatore modulo %
-      //stella a quando i è pari, se il resto di i/2 è 0
-      if(i % 2 == 0){
-        //stella tipo a
-        fill(255,255,150);
-        ellipse(starX,starY, 1); //1
-      }else if(i % 3 == 0){
-        //stella tipo b
-        //per ogni i divisibile per 3
-        fill(200,100,255);
-        ellipse(starX,starY, 1.5); //1.5
-      }else{
-        //stella tipo c
-        fill(255,255,100);
-        ellipse(starX,starY, 2.8); //2.8
-      }
+/*function drawSingleStar(i, starX, starY, random_transparency, random_size){
+}*/
 
-    }
-    //ciclo 2 specifica stella b, 40
-    //    for(let i = 0; i < 40; i++){
-    //  let starX = (i*37) % width + (i%3) * 5; //posizione stella collegata
-      // al valore dell'indice
-      // width è la larghezza dello schermo
-    //  let starY = ((i*73) % height) + (i%7);
-    //  fill(200,100,150);
-    //  ellipse(starX, starY, 1);
-    //}
-    //ciclo 3 specifica stella c, 40
-  pop();
+/*function drawStars(num_stars=120) { //di default sarà dato 120
+  for(let i=0; i < num_stars; i++) {
+    let starX = (i*37) % width + (1%3) * 5;
+    let starY = (i*73) % height + (i%7);
 
-  //aprire contesto di disegno, indispensabile
-  push();
+    let random_transparency = random(150, 255);
+    let random_size = random(2.8, 5.8);
 
+    drawSingleStar(i, starX, starY, random_transparency, random_size)
+  }
+}*/
+
+function drawRocketFromFile(xRocket, yRocket) {
+  image(rocket_img, xRocket, yRocket, 50, 50);
+}
+
+/*function drawRocket(xRocket, yRocket) {
   fill(220);
   stroke(40);
   //alternativa, disegnato dal centro
@@ -99,16 +76,36 @@ function draw() {
   strokeWeight(1);
   triangle(xRocket-40, yRocket+90, xRocket-20, yRocket+90, xRocket-80, yRocket+130);
   triangle(xRocket+40, yRocket+90, xRocket+80, yRocket+130, xRocket+20, yRocket+90);
+}*/
 
-  //finire contesto
-  pop();
-
-  //xRocket = (xRocket + 1) % xMax; //reset quando esce
-  yRocket = yRocket - 1;
+function moveRocket(yRocket, step=1) {
+    //xRocket = (xRocket + 1) % xMax; //reset quando esce
+    yRocket = yRocket - step;
   //quando la yRocket sarà oltre una certa soglia
-  let soglia = - (yMax/2);
-  if(yRocket < soglia){
-    yRocket = yMax;
-  }
+    let soglia = - (yMax * 0.6);
+    if(yRocket < soglia){
+      yRocket = yMax;
+    }
+    return yRocket; //così mi viene yRocket aggiornato
   //allora devo resettare la yRocket
+  }
+
+function draw() {
+  background("#9fcbf0ff");
+  // mostrare un testo bianco che
+  // dice le coordinate del mouse
+  // sul foglio da disegno
+  fill(0); //bianco
+  textSize(20);
+  //stringa, x, y
+  text("mouseX:"+ mouseX + ", \
+    mouseY: " + mouseY,20,20); // mouseX dice la posizione X del mouse all'interno del foglio
+
+  noStroke();
+  drawStarsFromFile();
+
+  //drawRocket(xRocket, yRocket);
+  drawRocketFromFile(xRocket, yRocket);
+
+  yRocket = moveRocket(yRocket, 1);
 }
